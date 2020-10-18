c7dee9f370eeeadca8a238133689853b4892eefb
BoringBoredom <BoringBoredom>
2020-10-18 12:59:44 +0200

    0.11

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 import json, os, requests, time, argparse, webbrowser
 
-current_version = 0.10
+current_version = 0.11
 try:
     r = requests.get("https://api.github.com/repos/BoringBoredom/Twitch-Chat-Downloader/releases/latest")
     new_version = float(r.json()["tag_name"])
@@ -21,7 +21,7 @@ time.sleep(0.08)
 data = response.json()
 session.headers['Authorization'] = f"Bearer {data['access_token']}"
 
-def format_time(time):
+def seconds_to_24h(time):
     hours = time // 3600
     minutes = (time - hours * 3600) // 60
     seconds = time - hours * 3600 - minutes * 60
@@ -43,36 +43,50 @@ def get_user_id(channel):
     user_id = data['data'][0]['id']
     return user_id
 
-def get_video_list(user_id, video_count):
-    response = session.get(f"https://api.twitch.tv/helix/videos?user_id={user_id}&type=archive&first={video_count}")
+def get_video_data(user_id= None, video_count= None, video_id= None):
+    if user_id != None:
+        response = session.get(f"https://api.twitch.tv/helix/videos?user_id={user_id}&type=archive&first={video_count}")
+    else:
+        response = session.get(f"https://api.twitch.tv/helix/videos?id={video_id}")
     time.sleep(0.08)
     data = response.json()
-    video_list = []
+    video_data = []
     for video in data['data']:
-        video_list.append(video['id'])
-    video_list.reverse()
-    return video_list
+        split = video['duration'].split('h')
+        hours = split[0]
+        split = split[1].split('m')
+        minutes = split[0]
+        seconds = split[1].split('s')[0]
+        duration = int(seconds) + int(minutes) * 60 + int(hours) * 3600
+        user_name = video['user_name']
+        video_data.append([video['id'], duration, user_name])
+    video_data.reverse()
+    return video_data
 
-def extract(file, data, video_id):
+def extract(file, data, video):
     for comment in data['comments']:
-        timestamp = format_time(int(comment['content_offset_seconds']))
-        file.write(f"[{video_id}] [{timestamp}] {comment['commenter']['display_name']}: {comment['message']['body']}\n")
-    print(f"{timestamp} - {video_id}")
+        seconds = int(comment['content_offset_seconds'])
+        timestamp = seconds_to_24h(seconds)
+        file.write(f"[{video[0]}] [{timestamp}] {comment['commenter']['display_name']}: {comment['message']['body']}\n")
+    return seconds
 
-def download_chat(video_list, channel= ""):
-    video_list_copy = video_list.copy()
-    if channel != "":
-        channel += " "
-    if video_list[0] == video_list[-1]:
-        file = open(f"{channel}{video_list[0]}.txt", "w", errors= 'ignore')
+def download_chat(video_data):
+    queue = []
+    finished = []
+    for video in video_data:
+        queue.append(video[0])
+    if queue[0] == queue[-1]:
+        file = open(f"{video_data[0][2]} {queue[0]}.txt", "w", errors= 'ignore')
     else:
-        file = open(f"{channel}{video_list[0]} to {video_list[-1]}.txt", "w", errors= 'ignore')
-    print(f"Pending: {video_list_copy}")
-    for video_id in video_list:
+        file = open(f"{video_data[0][2]} {queue[0]} to {queue[-1]}.txt", "w", errors= 'ignore')
+    for video in video_data:
+        video_id = video[0]
         response = session.get(f"https://api.twitch.tv/v5/videos/{video_id}/comments?content_offset_seconds=0")
         time.sleep(0.08)
         data = response.json()
-        extract(file, data, video_id)
+        timestamp = extract(file, data, video)
+        os.system('cls')
+        print(f"Queue:            {queue}\nFinished:         {finished}\n\nCurrent video:      {video[0]}   {round(timestamp / video[1] * 100)} %")
         cursor = None
         if '_next' in data:
             cursor = data['_next']
@@ -80,14 +94,18 @@ def download_chat(video_list, channel= ""):
             response = session.get(f"https://api.twitch.tv/v5/videos/{video_id}/comments?cursor={cursor}")
             time.sleep(0.08)
             data = response.json()
-            extract(file, data, video_id)
+            timestamp = extract(file, data, video)
+            os.system('cls')
+            print(f"Queue:            {queue}\nFinished:         {finished}\n\nCurrent video:      {video[0]}   {round(timestamp / video[1] * 100)} %")
             if '_next' in data:
                 cursor = data['_next']
             else:
                 cursor = None
                 file.write("\n\n")
-        del video_list_copy[0]
-        print(f"Pending: {video_list_copy}")
+        finished.append(queue[0])
+        del queue[0]
+    os.system('cls')
+    print(f"Queue:            {queue}\nFinished:         {finished}")
     file.close()
 
 parser = argparse.ArgumentParser()
@@ -96,15 +114,16 @@ parser.add_argument("-c", "--channel", help= "Twitch channel to download chatlog
 parser.add_argument("-n", "--number", help= "Number of past VODs to download chatlogs from (max 100). Parameter to be used in combination with -c")
 args = parser.parse_args()
 if args.video_id:
-    download_chat([args.video_id])
+    video_data = get_video_data(video_id= args.video_id)
+    download_chat(video_data)
 elif args.channel and args.number:
     user_id = get_user_id(args.channel)
-    video_list = get_video_list(user_id, args.number)
-    download_chat(video_list, args.channel)
+    video_data = get_video_data(user_id, args.number)
+    download_chat(video_data)
 elif args.channel:
     user_id = get_user_id(args.channel)
-    video_list = get_video_list(user_id, 1)
-    download_chat(video_list, args.channel)
+    video_data = get_video_data(user_id, 1)
+    download_chat(video_data)
 elif args.number:
     print("No channel specified.")
 else:
